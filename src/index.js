2bc62c02fbe96ea9a2b424bb021b351d21f57e8d
Rafael Valverde <omit.mv@gmail.com>
2024-04-19 11:28:33 -0300

    Corrigir porta de start para 8080 no package.json e adicionar suporte ao provedor de criptografia OpenSSL legado

--- src/index.js
+++ src/index.js
@@ -7,11 +7,9 @@ import { App } from './features/App'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 
-const VERSION_DESCRIPTION = 'v0.1.0'
-
 root.render(
   <React.StrictMode>
-    <App versionDescription={VERSION_DESCRIPTION} />
+    <App />
   </React.StrictMode>
 )
 
