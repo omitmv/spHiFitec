9e05a9af94ad3a9a59db9b164da42b6311fd02f2
Rafael Valverde <omit.mv@gmail.com>
2024-05-03 15:26:08 -0300

    Atualizar URL de envio de e-mail na função sendEmail

--- src/features/SendEmail/components/sendEmailController.js
+++ src/features/SendEmail/components/sendEmailController.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 export const sendEmail = async values => {
   axios
-    .post('https://apiwebview.azurewebsites.net/send-email', values)
+    .post('https://apiwebview.azurewebsites.net/util/send-email', values)
     .then(response => {
       console.log(response.data)
     })
